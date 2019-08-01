89fa69091d387aefae8d8ce4201620a252c51b55
beekalam <beekalam@gmail.com>
2019-08-01 11:51:52 +0430

    slicing with no end

--- app/pyslicer/PySlicer.php
+++ app/pyslicer/PySlicer.php
@@ -10,26 +10,39 @@ class PySlicer
      * @var array
      */
     private $arr;
+    private $len = 0;
 
     public function __construct($arr)
     {
         $this->arr = $arr;
+        $this->len = count($this->arr);
     }
 
-    public function _slice($start, $end = 0, $step = 1)
+    public function slicer($start, $end = null, $step = 1)
     {
         if (is_string($start)) {
             list($start, $end, $step) = $this->parseSlice($start);
-            return $this->__slice(intval($start), intval($end), intval($step));
+        } else if (is_null($end)) {
+            $end = $this->len;
+            return $this->_slice($start, $end, $step);
+        }
+
+        return $this->_slice($start, $end, $step);
+    }
+
+    private function _slice($start, $end, $step = 1)
+    {
+        if ($start < 0 || $end < 0 || $step < 0) {
+            return $this->sliceNegativeIndexes($start, $end, $step);
         } else {
-            return $this->__slice($start, $end, $step);
+            return $this->slicePositiveIndexes($start, $end, $step);
         }
     }
 
-    private function __slice($start, $end, $step = 1)
+    private function slicePositiveIndexes($start, $end, $step = 1)
     {
         $ans = [];
-        $len = count($this->arr);
+        $len = $this->len;
         for ($i = $start; $i < $end; $i += $step) {
             if ($i < $len) {
                 $ans[] = $this->arr[$i];
@@ -38,19 +51,35 @@ class PySlicer
         return $ans;
     }
 
+    private function sliceNegativeIndexes($start, $end, $step = 1)
+    {
+        return null;
+    }
+
     private function parseSlice($slice)
     {
         $parts = explode(':', $slice);
-        if (count($parts) == 2) {
-            $parts[] = 1;
+        if ($this->hasStartOnly($slice)) {
+            $parts[1] = $this->len;
+            $parts[2] = 1;
+        } else {
+            if (count($parts) == 2) {
+                $parts[] = 1;
+            }
         }
-        // var_dump($parts);
         return $parts;
+    }
+
+    private function hasStartOnly($slice)
+    {
+        $colon_count = substr_count($slice, ":");
+        $parts = explode(':', $slice);
+        return $colon_count == 1 && empty($parts[1]);
     }
 
     public static function slice($str, $arr)
     {
-        return (new PySlicer($arr))->_slice($str);
+        return (new PySlicer($arr))->slicer($str);
     }
 
 
--- tests/unit/PySlicerTest.php
+++ tests/unit/PySlicerTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Unit;
 
 use App\pyslicer\PySlicer;
-use function App\pyslicer\slice;
 use PHPUnit\Framework\TestCase;
+use function App\pyslicer\slice;
 
 class PySlicerTest extends TestCase
 {
@@ -17,41 +17,64 @@ class PySlicerTest extends TestCase
     function can_slice_with_positive_start_end()
     {
         $s = new PySlicer([1, 2, 3, 4, 5]);
-        $this->assertEquals([2, 3], $s->_slice(1, 3));
+        $this->assertEquals([2, 3], $s->slicer(1, 3));
     }
 
     /** @test */
     function can_slice_with_one_element()
     {
         $s = new PySlicer([1, 2, 3, 4, 5]);
-        $this->assertEquals([2], $s->_slice(1, 2));
+        $this->assertEquals([2], $s->slicer(1, 2));
     }
 
     /** @test */
     function can_slice_zero_elements()
     {
         $s = new PySlicer([1, 2, 3, 4, 5]);
-        $this->assertEquals([], $s->_slice(1, 1));
+        $this->assertEquals([], $s->slicer(1, 1));
+    }
+
+    /** @test */
+    function can_slice_zero_elements_with_index_zero()
+    {
+        $s = new PySlicer([1, 2, 3, 4, 5]);
+        $this->assertEquals([], $s->slicer(0, 0));
     }
 
     /** @test */
     function can_slice_with_step()
     {
         $s = new PySlicer([1, 2, 3, 4, 5]);
-        $this->assertEquals([1,3], $s->_slice(0, 4, 2));
+        $this->assertEquals([1, 3], $s->slicer(0, 4, 2));
     }
 
     /** @test */
     function can_accept_string_paramter()
     {
         $s = new PySlicer([1, 2, 3, 4, 5]);
-        $this->assertEquals([2], $s->_slice('1:2'));
+        $this->assertEquals([2], $s->slicer('1:2'));
     }
 
     /** @test */
     function can_use_helper_function_to_slice()
     {
-        $this->assertEquals([2],  PySlicer::slice("1:2",[1, 2, 3, 4, 5]));
+        $this->assertEquals([2], PySlicer::slice("1:2", [1, 2, 3, 4, 5]));
+    }
+
+    /** @test */
+    function can_slice_with_no_end()
+    {
+        $l = ['spam', 'Spam', 'SPAM!'];
+        $s = new PySlicer($l);
+        $this->assertEquals(['Spam', 'SPAM!'], $s->slicer(1));
+    }
+
+    /** @test */
+    function can_slice_with_no_end_with_input_string()
+    {
+        $l = ['spam', 'Spam', 'SPAM!'];
+        $s = new PySlicer($l);
+        $this->assertEquals(['Spam', 'SPAM!'], PySlicer::slice("1:", $l));
     }
 
 }
